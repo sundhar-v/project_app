e9f838b44e9588259d26b79296cef9eb63318b6c
Ira Kathane <itskozo@gmail.com>
2024-12-27 01:27:42 +0530

    restructing code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+
+const App = () => {
+  return (
+    "hello world"
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
