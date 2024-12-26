8c48c4c563052ac6f294fe8c20069f25c74721cb
Ira Kathane <itskozo@gmail.com>
2024-12-27 02:05:02 +0530

    font add

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
+const string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ligula ex, finibus eu enim non, semper mattis libero. Nulla volutpat efficitur mi, id ultricies metus lobortis vel. Donec feugiat orci sit amet lorem finibus viverra. Duis lectus dui, cursus sodales sapien et, euismod commodo felis. Nullam congue nulla id mauris luctus maximus. Donec ullamcorper tortor id dapibus interdum. Phasellus imperdiet orci ut sapien sagittis, non posuere eros varius. Nunc congue elit congue, pulvinar turpis sit amet, vehicula neque. In tempor accumsan consequat. Duis ullamcorper aliquet lectus, eu luctus purus iaculis a."
+
 const App = () => {
   return (
-    "hello world"
+    <>
+      <h1>{"Heading 1"}</h1>
+      <h2>{"Heading 2"}</h2>
+      <p>{string}</p>
+    </>
   );
 }
 
