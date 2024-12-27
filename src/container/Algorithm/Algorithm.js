62f4f66d591f63041a2a4b2e591c1f2d4a65eee4
Ira Kathane <itskozo@gmail.com>
2024-12-27 23:48:21 +0530

    eslint accomplished yayyy!!!

--- src/container/Algorithm/Algorithm.js
+++ src/container/Algorithm/Algorithm.js
@@ -1,5 +1,5 @@
 const Algorithm = () =>{
-    return <p>{"Body Text"}</p>
+  return <p>{"Body Text"}</p>
 }
 
 export default Algorithm
--- src/container/AppHeader/AppHeader.js
+++ src/container/AppHeader/AppHeader.js
@@ -1,10 +1,10 @@
 import Header from "../../component/Header/Header"
 
 const AppHeader = ({ currentPage, setCurrentPage }) =>{
-    return <Header 
+  return <Header 
     currentPage={currentPage}
     setCurrentPage={setCurrentPage}
-    />
+  />
 }
 
 export default AppHeader
