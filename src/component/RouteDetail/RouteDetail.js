6767a9b38a58d8a59e6597fba78f506b5637121f
sundhar <sundhar811@gmail.com>
2025-02-27 02:14:00 +0530

    route details - headers

--- src/component/RouteDetail/RouteDetail.js
+++ src/component/RouteDetail/RouteDetail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./RouteDetail.css"
 
 import RouteDetailPropTypes from './RouteDetail.propTypes';
 
@@ -6,10 +7,38 @@ const RouteDetail = ({routeData, currentRoute}) => {
   console.log(routeData)
   return (
     <>
-      <div className="tile tile-centered">
+      <div className="tile tile-centered routeHeader">
         <div className="tile-content">
-          <div className="tile-title">{routeData.routeName.join(" -> ")}</div>
-          <small className="tile-subtitle text-gray">{"Route" + currentRoute}</small>
+          <small className="tile-subtitle text-error">{"Route " + currentRoute}</small>
+          <div className="h5 text-primary">{routeData["routeName"].join(" -> ")}</div>
+        </div>
+      </div>
+      <div className="container routeSubHeader">
+        <div className="columns">
+          <div className="column col-xs-4">
+            <div className="tile tile-centered">
+              <div className="tile-content">
+                <div className="h5 text-success">{"Route Cost"}</div>
+                <div className="h6">{Math.round(routeData["routeCost"])}</div>
+              </div>
+            </div>
+          </div>
+          <div className="column col-xs-4">
+            <div className="tile tile-centered">
+              <div className="tile-content">
+                <div className="h5 text-success">{"Total Demand"}</div>
+                <div className="h6">{routeData["routeDemand"]}</div>
+              </div>
+            </div>
+          </div>
+          <div className="column col-xs-4">
+            <div className="tile tile-centered">
+              <div className="tile-content">
+                <div className="h5 text-success">{"Total Pickup"}</div>
+                <div className="h6">{routeData["routePickup"]}</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
