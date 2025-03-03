0d7b6f481151a62eccc0948be3a70e36bf53b457
Ira Kathane <itskozo@gmail.com>
2025-03-03 13:58:28 +0530

    algo fix

--- src/utils/simple_cw.js
+++ src/utils/simple_cw.js
@@ -149,9 +149,7 @@ const isRouteFeasible = (
   let currentNode = 0
   for (const node of route) {
     // Time window feasibility
-    currentTime = currentNode !== 0 
-      ? currentTime + (distanceMatrix[currentNode][node] / averageVehicleSpeed) * 60
-      : Math.max(currentTime + (distanceMatrix[currentNode][node] / averageVehicleSpeed) * 60, inputData.timeWindows[node-1][0])
+    currentTime = Math.max(currentTime + (distanceMatrix[currentNode][node] / averageVehicleSpeed) * 60, inputData.timeWindows[node-1][0])
     // if arrival time at node exceeds delivery end or made to wait more than tolerable waiting limit then the route is not feasible
     // excluding this check if current node = 0 (depot)
     if (currentNode !== 0 && (currentTime > deliveryEnd || (inputData.timeWindows[node-1][0] - currentTime) > maximumWaitingTime || currentTime > inputData.timeWindows[node-1][1])) {
